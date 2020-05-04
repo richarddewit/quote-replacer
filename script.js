d7568d62093b97a13d5acee6512033514c854452
Richard de Wit <henk.exe@gmail.com>
2020-05-04 16:05:39 +0200

    Add html and script

---
+++ script.js
@@ -0,0 +1,55 @@
+(function() {
+    var quotes = {
+        fancy: "“”",
+        double: '"',
+        single: "'",
+        backtick: "`",
+        french: "«»",
+    };
+    var from = "fancy";
+    var to = "double";
+
+    var form = document.forms.quoteform;
+    var origin = form.origin;
+    var result = form.result;
+
+    form.from.forEach(function(radio) {
+        if (radio.value === from) {
+            radio.checked = true;
+        }
+
+        radio.addEventListener("change", function() {
+            from = this.value;
+            replaceQuotes();
+        });
+    });
+
+    form.to.forEach(function(radio) {
+        if (radio.value === to) {
+            radio.checked = true;
+        }
+
+        radio.addEventListener("change", function() {
+            to = this.value;
+            replaceQuotes();
+        });
+    });
+
+    form.addEventListener("submit", function(e) {
+        e.preventDefault();
+        replaceQuotes();
+    });
+
+    origin.addEventListener("keyup", function(e) {
+        replaceQuotes();
+    });
+
+    origin.addEventListener("paste", function(e) {
+        setTimeout(replaceQuotes, 0);
+    });
+
+    function replaceQuotes() {
+        var re = new RegExp("[" + quotes[from] + "]", "g");
+        result.value = origin.value.replace(re, quotes[to]);
+    }
+})();
